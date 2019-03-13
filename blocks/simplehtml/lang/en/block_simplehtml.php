4a66299d43697197e90d2b00367a44aec47f3342
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-12 19:28:49 -0600

    Prueba de Concepto: block_simplehtml

---
+++ blocks/simplehtml/lang/en/block_simplehtml.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+ *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+ *   SUBJECT:     _
+ *   PROFESSOR:   _
+ *
+ * DESARROLLADORES: Daniel Ortega
+ *
+ * PRACTICE _:  TITULO DE LA PRACTICA
+ *               - DESCRIPCION
+ *		
+*/
+
+$string['pluginname'] = 'Simple HTML block';
+$string['simplehtml'] = 'Simple HTML';
+$string['simplehtml:addinstance'] = 'Add a new simple HTML block';
+$string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
---
+++ blocks/simplehtml/version.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+ *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+ *   SUBJECT:     _
+ *   PROFESSOR:   _
+ *
+ * DESARROLLADORES: Daniel Ortega
+ *
+ * PRACTICE _:  TITULO DE LA PRACTICA
+ *               - DESCRIPCION
+ *		
+*/
+
+$plugin->component = 'block_simplehtml';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->version = 2011062800;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
