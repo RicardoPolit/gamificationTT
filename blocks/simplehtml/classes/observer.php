e7d96e34ba528d787833db0892f34ffd77d80c71
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-21 13:24:45 -0600

    Updating text for SimpleHTML Block

--- blocks/simplehtml/classes/observer.php
+++ blocks/simplehtml/classes/observer.php
@@ -20,11 +20,16 @@ class block_simplehtml_observer {
     public static function update(\core\event\base $event) {
         // This code works when put inside the corresponding 'store' function in blocks/recent_activity/classes/observer.php
         
-        $_SESSION['Gamedle'] = array();
+        if(!isset($_SESSION['Gamedle']['tmp'])){
+            $_SESSION['Gamedle'] = array();
+            $_SESSION['Gamedle']['tmp'] = "";
+        }
         $_SESSION['Gamedle']['user_lvl'] = 3;
         $_SESSION['Gamedle']['user_xp'] = 300;
         
         $json_data = json_encode($event->get_data());
+        $_SESSION['Gamedle']['tmp'] .= "EVENT: \".$json_data.\"";
+        
         echo("<script>console.log('EVENT: ".$json_data."');</script>");
     }
 }
