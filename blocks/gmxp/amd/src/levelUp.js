5a75a9d515ced1f1107b4f30e603a2719d6d91aa
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-04-13 12:44:52 -0500

    Added animation of popup

--- blocks/gmxp/amd/src/levelUp.js
+++ blocks/gmxp/amd/src/levelUp.js
@@ -2,32 +2,43 @@
 define(['block_gmxp/experienceUp'], function(experienceUp) {
 
     var popup = $("#gmxp-popup")[0];
+    var content = $('#gmxp-content')[0];
     var flag = true;
-    
-    function hidePopup(params){
+
+    function hidePopup(params) {
         popup.style.display = "none";
         experienceUp.parametrized($(".gmxp-progress")[0],params);
     }
- 
+
+    function showPopup() {
+        var width = 0;
+        popup.style.display = "block";
+        /*var id = setInterval(draw,30);
+
+        function draw() {
+            width++;
+            content.style.width = width+'%';
+            if (width >= 40) {
+                clearInterval(id);
+            }
+        }*/
+    }
+
     return {
-        init: function(params){
-            params = Object
-            params.inicio = 12;
-            params.final  = 134;
-            
-            popup.style.display = "block";
+        init: function(params) {
+
+            showPopup();
             $("#gmxp-popup").on("click", function(){
-                hidePopup(params)
+                hidePopup(params);
             });
             window.onkeydown = function(e) {
                 if(flag){
-                    flag = false
+                    flag = false;
                     hidePopup(params);
                     if (e.keyCode == 32 && e.target == document.body)
                         e.preventDefault();
                 }
             };
         }
-        
     };
 });
