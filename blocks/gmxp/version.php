9e16425cbd745f4204bcbe086b8ec35c005e4333
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-04-13 12:49:40 -0500

    Added dependency to local plugin

--- blocks/gmxp/version.php
+++ blocks/gmxp/version.php
@@ -12,6 +12,15 @@
  *		
 */
 
+/*
+ * Read the docs:
+ *   https://docs.moodle.org/dev/version.php
+ */
+
 $plugin->component = 'block_gmxp';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version =  2019040321;      // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version =  2019041313;     // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400;     // YYYYMMDDHH (This is the release version for Moodle 2.0)
+
+$plugin->dependencies = array(
+    'local_gamedlemaster' => ANY_VERSION // Should Specify the version
+);
