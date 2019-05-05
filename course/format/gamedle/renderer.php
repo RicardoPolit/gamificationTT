f3e2b2596bcae6566ea4bb56afc3e7877a5916ca
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-05-05 14:04:04 -0500

    Added input for mission experience

--- course/format/gamedle/renderer.php
+++ course/format/gamedle/renderer.php
@@ -15,5 +15,22 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot.'/course/format/topics/renderer.php');
 
 class format_gamedle_renderer extends format_topics_renderer{
-	
+
+    public function print_multiple_section_page($course, $sections, $mods, $modnames, $modnamesused){
+        parent::print_multiple_section_page($course, $sections, $mods, $modnames, $modnamesused);
+
+        /* TODO: Add button for save experience points configurations */
+    }
+
+    protected function section_right_content($section, $course, $onsectionpage) {
+
+        global $PAGE;
+        $o = parent::section_right_content($section,$course,$onsectionpage);
+
+        $input = "";
+        if($PAGE->user_is_editing())
+            $input = "<divº>Mission XP: <input type='text' placeholder='1000'></divº>";
+        
+        return $o.$input;
+    }
 }
