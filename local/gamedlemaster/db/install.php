b98aca62ca4eb15d9b9958da5012e0d5ca66bf69
DanielOrtegaz <dortegaz1500@alumno.ipn.mx>
2019-09-25 21:16:17 -0500

    Update: Dev - Gamedlemaster install.php
    Se hicieron los siguientes cambios en el plugin
    local/gamedlemaster:
    
        La variable global **DB** se pasa a la funcion
        correspondiente
    
        Se elimina el contador i, para realizar la prueba
        de imprimir el primer registro
    
    To does:
    
      - Crear el logger

--- local/gamedlemaster/db/install.php
+++ local/gamedlemaster/db/install.php
@@ -8,27 +8,20 @@
 
 function xmldb_local_gamedlemaster_install()
 {
-    global $DB;
-
     copiarUsuarios();
 }
 
 function copiarUsuarios()
 {
+    global $DB;
     $result = $DB->get_records("user", array()); // get all records in mdl_user table
-    $i = 0;
+
     foreach ($result as $record) {
         $data = new stdClass();
         $data->mdl_id_usuario = $record->id;
-        $data->nivell_actual = 1;
+        $data->nivel_actual = 1;
         $data->experiencia_actual = 0;
-        //echo $record->username;
-        if ($i == 0) {
-            echo 'Así se ve un objeto de tipo usuario:\n';
-            echo var_dump($record);
 
-        }
         $DB->insert_record("gmdl_usuario", $data);
-        $i = $i + 1;
     }
 }
