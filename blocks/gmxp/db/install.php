78d13e5d71d1454204d02e188ed785f218dce435
Daniel Ortega <dortegaz1500@alumno.ipn.mx>
2019-10-17 00:15:09 -0500

    Add: Mod. Experiencia - block/gmxp/db/install.php
    Se añade el archivo install.php para los valores por defecto de los
    external pages

---
+++ blocks/gmxp/db/install.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+ *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+ *   SUBJECT:     _
+ *   PROFESSOR:   _
+ *
+ * DESARROLLADORES: Daniel Ortega
+ *
+ * PRACTICE _:  TITULO DE LA PRACTICA
+ *               - DESCRIPCION
+ *		
+*/
+
+define('PLUGIN', 'block_gmxp');
+
+    /**
+     * Constants that defines the default values
+     */
+
+    local_gamedle_master_log::info("Set default value of ...", "gmxp"); 
+?>
