104303702320b728e8cc6426865357ff7cc6e25d
Daniel Ortega <dortegaz1500@alumno.ipn.mx>
2019-10-19 02:04:04 -0500

    Update: Mod Experiencia - block gmxp visual Settings Form

--- blocks/gmxp/classes/visualsettingsform.php
+++ blocks/gmxp/classes/visualsettingsform.php
@@ -9,7 +9,7 @@
  * DESARROLLADORES: Daniel Ortega (GitLab/Github @DanielOrtegaZ)
 */
 
-define('PLUGIN', 'block_gmxp');
+
 
 /**
  * 
@@ -17,15 +17,17 @@ define('PLUGIN', 'block_gmxp');
  */
 class block_gmxp_visualsettingsform extends moodleform {
 
+    const PLUGIN = 'block_gmxp';
     protected function definition() {
 
         $mform = $this->_form; // Inherited from moodle form
-        $mform->setDisableShortforms(true); // TODO CHECK WHY?
+        // $mform->setDisableShortforms(true); // TODO CHECK WHY?
 
         $this->create_definition();
         $this->create_help_messages();
         $this->create_form_restrictions();
         $this->set_default_values();
+
         $this->add_action_buttons();
     }
 
@@ -34,8 +36,8 @@ class block_gmxp_visualsettingsform extends moodleform {
         $mform = $this->_form;
 
         $mform->addElement('text',
-            get_string('SYS_SETTINGS_VISUAL_TITLE', PLUGIN),
-            get_string('VISUAL_SETTING_TEXT_TITLE', PLUGIN));
+            get_string('SYS_SETTINGS_VISUAL_TITLE', self::PLUGIN),
+            get_string('VISUAL_SETTING_TEXT_TITLE', self::PLUGIN));
     }
 
     private function create_help_messages() {
@@ -47,21 +49,33 @@ class block_gmxp_visualsettingsform extends moodleform {
 
         $mform = $this->_form;
 
-        $key = get_string('SYS_SETTINGS_VISUAL_TITLE', PLUGIN);
+        $key = get_string('SYS_SETTINGS_VISUAL_TITLE', self::PLUGIN);
+        $mform->setType($key, PARAM_TEXT);
         $mform->addRule($key, get_string('required'), 'required', null, 'client');
-        $mform->addRule($key, get_string('maxlength', PLUGIN, 40), 'maxlength', null, 'client');
-
-        local_gamedlemaster_log::info(
-            get_string('maxlength', PLUGIN, '40')
-        );
+        $mform->addRule($key, get_string('maxlength', self::PLUGIN, 30),
+            'maxlength', 30, 'client');
     }
 
     private function set_default_values() {
 
         $mform = $this->_form;
 
-        $key = get_string('SYS_SETTINGS_VISUAL_TITLE', PLUGIN);
-        $mform->setDefault($key, get_config(PLUGIN, $key));
+        $key = get_string('SYS_SETTINGS_VISUAL_TITLE', self::PLUGIN);
+        $mform->setDefault($key, get_config(self::PLUGIN, $key));
+    }
+
+    public function validation($data, $files) {
+        return array();
+    }
+
+    public function submitChanges($changes) {
+        $keys = get_object_vars($changes);
+        unset($keys['submitbutton']);
+
+        foreach ($keys as $key => $value) {
+            local_gamedlemaster_log::info(
+            "set_config('{$key}', {$value}, {self::PLUGIN});",'SUBMIT');
+        }
     }
 }
 
--- blocks/gmxp/settings/visual_settings.php
+++ blocks/gmxp/settings/visual_settings.php
@@ -8,6 +8,18 @@ define('PLUGIN', 'block_gmxp');
 admin_externalpage_setup( get_string('SYS_SETTINGS_VISUAL', PLUGIN) );
 
 $form = new block_gmxp_visualsettingsform(); 
+$output = '';
+
+if ($form->is_cancelled()) {
+    local_gamedlemaster_log::info('cancelled');
+} /*else if ($form->is_submitted() && !$form->is_validated()) {
+
+}*/ else if ($changes = $form->get_data()) {
+    $form->submitChanges($changes);
+
+} else {
+    local_gamedlemaster_log::info('not validated');
+}
 
 echo $OUTPUT->header();
 echo $form->render();
