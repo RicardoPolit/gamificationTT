43237ac886692a741d47a83f4afcd1f9d56b430e
Daniel Ortega <dortegaz1500@alumno.ipn.mx>
2019-10-22 04:14:39 -0500

    Update: Mod. Experiencia - Se agrega condicion de si el módulo esta activado

--- blocks/gmxp/settings/visual_settings.php
+++ blocks/gmxp/settings/visual_settings.php
@@ -5,7 +5,17 @@ require_once($CFG->libdir.'/adminlib.php');
 
 $PLUGIN = 'block_gmxp';
 $MESSAGE = '';
+$CONTENT = '';
+$experience_activated = get_config($PLUGIN,
+  get_string('SYS_SETTINGS_GENERAL_ACTIVATED', $PLUGIN));
+
+
+/**
+ * Si el módulo de experiencia se encuentra activado
+ * procede de forma natural al formulario de configuraciones
+ */ 
 admin_externalpage_setup( get_string('SYS_SETTINGS_VISUAL', $PLUGIN) );
+if ($experience_activated) {
 
     $form = new block_gmxp_visualsettingsform(); 
 
@@ -28,7 +38,37 @@ admin_externalpage_setup( get_string('SYS_SETTINGS_VISUAL', $PLUGIN) );
         local_gamedlemaster_log::info('not validated');
     }
 
+    $CONTENT = $form->render();
+
+/**
+ * Si el módulo de experiencia no se encuentra activado
+ * entonces se imprime la pantalla de error
+ *
+ * Docs https://docs.moodle.org/dev/html_writer
+ */ 
+} else {
+
+    $MESSAGE = $OUTPUT->notification(
+                 get_string('SETTINGS_EXPERIENCE_DISABLED', $PLUGIN));
+
+    $text = get_string('SETTINGS_EXPERIENCE_DISABLED_REDIRECT', $PLUGIN);
+    $category = get_string('SYS_SETTINGS_CATEGORY', $PLUGIN);
+
+    $CONTENT = html_writer::start_tag('a', array(
+        'href' => "{$CFG->wwwroot}/admin/category.php?category={$category}"
+    ));
+
+    $CONTENT .= html_writer::empty_tag('input', array(
+        'class' => "btn btn-primary",
+        'value' => $text,
+        'style' => 'margin:auto; display: block'
+    ));
+
+    $CONTENT .= html_writer::end_tag('a');
+
+}
+
 echo $OUTPUT->header();
 echo $MESSAGE;
-echo $form->render();
+echo $CONTENT;
 echo $OUTPUT->footer();
