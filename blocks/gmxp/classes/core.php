694ca62c26a6d8a02c8e09a04e1e28c5e0cebd7e
Daniel Ortega <dortegaz1500@alumno.ipn.mx>
2019-10-26 18:53:51 -0500

    Add: Mod Experiencia. Constant class (block/gmxp/classes/core) & Update settings.php

---
+++ blocks/gmxp/classes/core.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+ *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+ *
+ *   PROJECT:     Gamedle
+ *   PROFESSOR:   Sandra Bautista, Andres Catalan.
+ *
+ * DESARROLLADORES: Daniel Ortega (GitLab/Github @DanielOrtegaZ)
+*/
+
+class block_gmxp_core {
+
+    const CATEGORY = 'block_gmxp_category';
+
+    const SETTINGS_GENERAL = "block_gmxp_general_settings";
+    const ACTIVATED = "activated";
+    const EVENTS = "events";
+
+    const SETTINGS_VISUAL = "block_gmxp/visual_settings";
+    const PATH_SETTINGS_VISUAL = "/blocks/gmxp/settings/visual_settings.php";
+
+    const VISUAL_TITLE = "title";
+    const VISUAL_DESCRIPTION = "description";
+    const VISUAL_MESSAGE = "message";
+    const VISUAL_COLORLVL = "colorLvl";
+    const VISUAL_COLORBAR = "colorBar";
+    const VISUAL_IMAGE = "image";
+    const VISUAL_IMAGE_PATH = "/blocks/gmxp/pix/";
+
+    const SETTINGS_SCHEME = "block_gmxp/scheme_settings";
+    const PATH_SETTINGS_SCHEME = '/blocks/gmxp/settings/scheme_settings.php';
+    const SCHEME_INCREMENT = "increment";
+    const SCHEME_VALUE = "incrementValue";
+    const SCHEME_LEVELXP = "levelXP";
+    const SCHEME_COURSEXP = "courseXP";
+
+    const SETTINGS_EVENTS = "block_gmxp/events_settings";
+    const PATH_SETTINGS_EVENTS = "/blocks/gmxp/settings/events_settings";
+}
+?>
