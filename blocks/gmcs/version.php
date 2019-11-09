0fa9d98b54b58dedd4fda7ab1d8fe7455a7dfddc
DanielOrtegaz <dortegaz1500@alumno.ipn.mx>
2019-11-08 21:39:37 -0600

    Update: Mod. Financiero. Settings y bloque financiero

--- blocks/gmcs/version.php
+++ blocks/gmcs/version.php
@@ -8,4 +8,12 @@
  *
  * DESARROLLADORES: Daniel Ortega (GitLab/Github @DanielOrtegaZ)
 */
+
+$plugin->component = 'block_gmcs';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL
+$plugin->version =  2019072516;     // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2010112400;     // YYYYMMDDHH (This is the release version for Moodle 2.0)
+
+$plugin->dependencies = array(
+    'local_gamedlemaster' => 2019042101 // Should Specify the version
+);
 ?>
