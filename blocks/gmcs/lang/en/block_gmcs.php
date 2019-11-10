b3c70264af16983179dd9bf5b37057b565e7a673
Daniel Ortega <dortegaz1500@alumno.ipn.mx>
2019-11-09 23:10:22 -0600

    Update: Mod Financiero. - Scheme Settings
    Se crea la condicionante de que si el módulo de financiero no está
    habilitado entonces se muestre el mensaje de error con el botón para
    redirigir a las configuraciones con el propósito de activarlo.

--- blocks/gmcs/lang/en/block_gmcs.php
+++ blocks/gmcs/lang/en/block_gmcs.php
@@ -14,11 +14,28 @@ $string['gmcs'] = $string['pluginname'];
 $string['gmcs:addinstance']   = "Add a new {$string['pluginname']} block";
 $string['gmcs:myaddinstance'] = "Add a new {$string['pluginname']} block to the My Moodle page";
 
+// ==== SETTINGS =====================================
 $string['SETTINGS_CATEGORY'] = "Gamedle: Finance Module";
 $string['SETTINGS_GENERAL'] = "General Setting Up";
 $string['SETTINGS_SCHEME']  = "Currency Setting Up";
+
+// ==== GENERAL SETTINGS =====================================
 $string['SETTINGS_GENERAL_HEADER'] = "Choose the main features";
 $string['SETTINGS_GENERAL_HEADER_DESC'] = "TODO: Write header description";
 $string['SETTINGS_GENERAL_ENABLED'] = "Enable hte Currency System ";
 $string['SETTINGS_GENERAL_ENABLED_DESC'] = "TODO: Write the enabled description";
+
+// ==== SCHEME SETTINGS =====================================
+$string['SETTINGS_SCHEME_HEADER'] = "Customize the currency system";
+$string['SETTINGS_SCHEME_DESC'] = 
+    "Control how does the currency system works. Specify the equivalence rules
+    between silver coins and gold coins.";
+
+// === FORM|SETTINGS ERRORS ==========================================
+$string['SETTINGS_CURRENCY_DISABLED_REDIRECT'] = "Enabled Module";
+$string['SETTINGS_CURRENCY_DISABLED'] =
+    "The currency module is deactivated in the *{$string['SETTINGS_GENERAL']}* page 
+    if you want to access this configuration you must enable the currency module";
+
+
 
--- blocks/gmcs/settings/scheme_settings.php
+++ blocks/gmcs/settings/scheme_settings.php
@@ -16,20 +16,18 @@ require_once($CFG->libdir.'/adminlib.php');
 $PLUGIN = 'block_gmcs';
 admin_externalpage_setup(block_gmcs_core::SETTINGS_SCHEME);
 
-$CONTENT = 'HOLA MUNDO';
-/*
-$form = new block_gmxp_schemesettingsform();
-$experience_activated = get_config($PLUGIN, block_gmxp_core::ACTIVATED);
+$CONTENT = '';
+$form = new block_gmcs_schemesettingsform();
+$currency_activated = get_config($PLUGIN, block_gmcs_core::ACTIVATED);
 
 /**
- * Si el módulo de experiencia se encuentra activado
+ * Si el módulo financiero se encuentra activado
  * procede de forma natural al formulario de configuraciones
  */ 
 
-/*
-if($experience_activated){
+if($currency_activated){
 
-    if ($form->is_cancelled()) {
+    /*if ($form->is_cancelled()) {
         $form->go_site_administration();
 
     } else if ($changes = $form->get_data()) {
@@ -37,19 +35,19 @@ if($experience_activated){
 
     } else {
         local_gamedlemaster_log::info('not validated');
-    }
+    }*/
 
     $CONTENT = $form->render();
 
 } else {
 
-    $category = block_gmxp_core::CATEGORY;
-    $error =    get_string('SETTINGS_EXPERIENCE_DISABLED', $PLUGIN);
-    $text =     get_string('SETTINGS_EXPERIENCE_DISABLED_REDIRECT', $PLUGIN);
+    $category = block_gmcs_core::CATEGORY;
+    $error =    get_string('SETTINGS_CURRENCY_DISABLED', $PLUGIN);
+    $text =     get_string('SETTINGS_CURRENCY_DISABLED_REDIRECT', $PLUGIN);
     $link = "{$CFG->wwwroot}/admin/category.php?category={$category}";
 
     $CONTENT = $form->render_problem($error, $text, $link);
-}*/
+}
 
 echo $OUTPUT->header();
 echo $CONTENT;
